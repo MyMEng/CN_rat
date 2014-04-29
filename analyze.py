772ea9ecfdb887047255984397438f1338615dc7
kacper <ks1591@bristol.ac.uk>
2014-04-29 03:27:12 +0200

    To begin eith...

--- analyze.py
+++ analyze.py
@@ -1,3 +1,17 @@
+#! /usr/bin/python
+
+# import
+from math import ceil
+from pprint import pprint
+import matplotlib.pyplot as plt
+
+# Round up to nearest 10
+def round10( x ) :
+	if x%10 == 0 :
+		return x
+	else :
+		return int( ceil( x/10.0 ) * 10 )
+
 # Load data
 #  for loading neuron 1 - 4 and time
 N = []
@@ -13,4 +27,48 @@ for i in ['x', 'y'] :
 f = open("data/time.csv")
 T = map( lambda x: float(x.strip()), f.readlines() )
 
+# Get maze size
+Xmin = round10( min( D[0] ) )
+Xmax = round10( max( D[0] ) )
+Ymin = round10( min( D[1] ) )
+Ymax = round10( max( D[1] ) )
+
+# Get number of cells and Divide into them
+Visited = []
+Xsize = Xmax /10
+Ysize = Ymax /10
+for i in range(Xsize) :
+	Visited.append([])
+	for j in range(Ysize) :
+		Visited[i].append([])
+
+AtPosition = []
+PositionChange = [-1]
+# put times at which the mouse was at given square
+for i, (x, y) in enumerate( zip(D[0], D[1]) ) :
+	currentX = int( x/10 )
+	currentY = int( y/10 )
+	Visited[currentX][currentY].append(T[i])
+	AtPosition.append( (currentX, currentY) )
+	# if last position differ from current one add | otherwise skip
+	if PositionChange[-1] != (currentX, currentY) :
+		PositionChange.append( (currentX, currentY) )
+
+# print PositionChange
+plt.figure(1)
+# plt.xlabel("time 't' in seconds")
+# plt.ylabel("voltage V in Volts")
+# plt.title('Spiking Integrate-and-Fire Model')
+# plt.axis([ 0, 1, -0.075, -0.035 ])
+# plt.savefig("figure1.png", dpi=300, pad_inches=0.2)
+for (x, y) in PositionChange :
+	plt.plot(x,y)
+plt.show()
+
+# or give just order of squares as a list --- which shuare [ square(1,1), (1,2), etc.. ]
+
+# check the position and remember whether mouse was already here if so check
+# the change of firing rate
+
+# split the maze into bricks
 
